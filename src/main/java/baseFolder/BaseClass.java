2aa18b56b3c66306dee530b72d7c1fbec93fb132
Navaneeth <navaneeth.tester@gmail.com>
2023-09-05 12:47:55 +0530

    Chrome and Firefox Options code added.

--- src/main/java/baseFolder/BaseClass.java
+++ src/main/java/baseFolder/BaseClass.java
@@ -3,6 +3,7 @@ package baseFolder;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
@@ -10,21 +11,40 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	public WebDriver driver;
+	private static ChromeOptions chromeOption;
+	private static FirefoxOptions firefoxOption;
 	public static final String screenShotPath = "reports/screenShots";
 
+	private void setChromeOptions() {
+		chromeOption=new ChromeOptions();
+		chromeOption.setImplicitWaitTimeout(Duration.ofSeconds(2));
+		chromeOption.setPageLoadTimeout(Duration.ofSeconds(20));
+		chromeOption.addArguments("disable-infobars").addArguments("disable-popup-blocking");
+	}
+	
+	private void setFirefoxOptions() {
+		firefoxOption=new FirefoxOptions();
+		firefoxOption.setImplicitWaitTimeout(Duration.ofSeconds(2));
+		firefoxOption.setPageLoadTimeout(Duration.ofSeconds(20));
+	}
+	
 	public WebDriver openBrowser(String browserName) {
 		if (browserName.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			setChromeOptions();
+			driver = new ChromeDriver(chromeOption);
 		} else if (browserName.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
+			setFirefoxOptions();
+			driver = new FirefoxDriver(firefoxOption);
 		} else {
 			System.out.println(browserName + " ERROR...");
 		}
