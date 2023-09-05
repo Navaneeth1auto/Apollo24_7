82ca2baf309ac68adcc9e36f06d0886334221bf1
Navaneeth <navaneeth.tester@gmail.com>
2023-09-05 11:22:46 +0530

    Web Driver initialization code added to base class.

---
+++ src/main/java/baseFolder/BaseClass.java
@@ -0,0 +1,37 @@
+package baseFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	public WebDriver driver;
+
+	public WebDriver openBrowser(String browserName) {
+		if (browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		} else {
+			System.out.println(browserName + " ERROR...");
+		}
+		return driver;
+	}
+
+	public void openApplication() {
+		driver.get("https://www.apollo247.com/");
+	}
+
+}
