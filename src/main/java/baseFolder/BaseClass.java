33260373f6590e4009eaf7f28bf353919873d4c1
Navaneeth <navaneeth.tester@gmail.com>
2023-09-05 11:27:48 +0530

    ScreenShot code added to base class.

--- src/main/java/baseFolder/BaseClass.java
+++ src/main/java/baseFolder/BaseClass.java
@@ -16,6 +16,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 	public WebDriver driver;
+	public static final String screenShotPath = "reports/screenShots";
 
 	public WebDriver openBrowser(String browserName) {
 		if (browserName.equalsIgnoreCase("chrome")) {
@@ -34,4 +35,16 @@ public class BaseClass {
 		driver.get("https://www.apollo247.com/");
 	}
 
+	public String screenShot() {
+		String timeStamp = new SimpleDateFormat("hh_mm_ss_SSS_dd_MM_yyyy").format(new Date());
+		String destPath = screenShotPath + "/" + timeStamp + ".png";
+
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(scrFile, new File(destPath));
+		} catch (IOException e) {
+			System.out.println("ERROR in screen shot file creation : " + e.getMessage());
+		}
+		return destPath;
+	}
 }
